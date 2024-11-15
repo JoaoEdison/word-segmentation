20d04012da7e6352d1d5c069e20ab09de9feab88
JoaoEdison <joaoedisonmanica@gmail.com>
2024-11-15 00:30:28 -0300

    Modelo CTC construído.

--- segmentador.py
+++ segmentador.py
@@ -186,17 +186,18 @@ word_dist_x = pr.ffi.new('float *', 7.0)
 block_dist_y = pr.ffi.new('float *', 70.0)
 block_dist_x = pr.ffi.new('float *', 70.0)
 
+# Referências:
+# https://medium.com/@natsunoyuki/ocr-with-the-ctc-loss-efa62ebd8625
+# https://keras.io/examples/vision/captcha_ocr
+# https://www.tensorflow.org/tutorials/keras/save_and_load
+
 EPOCHS = 50
-batch_size = 5
+batch_size = 20
 width_input = 64
 height_input = 16
 max_len = -1
+alphabet_to_ind = None
 ind_to_alphabet = None
-
-# Referências:
-# https://medium.com/@natsunoyuki/ocr-with-the-ctc-loss-efa62ebd8625
-# https://keras.io/examples/vision/captcha_ocr/#model
-# https://www.tensorflow.org/tutorials/keras/save_and_load
 
 def create_model(num_glyphs):
     labels = layers.Input(shape=(None,), name='label', dtype='float32')
@@ -213,130 +214,146 @@ def create_model(num_glyphs):
     model.compile(optimizer=optimizers.Adam())
     return model
 
-def get_net():
-    model_is_saved = os.path.isfile('modelo.keras')
-    files = []
-    directory = "test-images" if model_is_saved else "Page_Level_Training_Set"
-    for file in os.listdir(directory):
-        if file.endswith(".jpg") or file.endswith(".jpeg") or \
-                file.endswith(".png"):
-            file_name = os.path.join(directory, file)
-            text_file = os.path.join(directory, file.split('.')[0] + ".txt")
-            with open(text_file, "r") as f:
-                content = f.read()
-                files.append((file_name, content))
-            if len(files) == 10:
-                break
+model_is_saved = os.path.isfile('modelo.keras')
+files = []
+directory = "test-images" if model_is_saved else "Page_Level_Training_Set"
+for file in os.listdir(directory):
+    if file.endswith(".jpg") or file.endswith(".jpeg") or \
+            file.endswith(".png"):
+        file_name = os.path.join(directory, file)
+        text_file = os.path.join(directory, file.split('.')[0] + ".txt")
+        with open(text_file, "r") as f:
+            content = f.read()
+            files.append((file_name, content))
+        if len(files) == 10:
+            break
 
-    if model_is_saved:
-        alphabet = ['"', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7',
-                '8', '9', ':', 'A', 'B', 'C', 'E', 'F', 'H', 'I', 'J', 'K', 'L',
-                'R', 'S', 'U', 'W', 'Y', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v',
-                'w', 'x', 'y', 'z']
-        max_len = 16
-        alphabet_to_ind = layers.StringLookup(vocabulary=list(alphabet))
-        ind_to_alphabet = layers.StringLookup(vocabulary=alphabet_to_ind.get_vocabulary(), invert=True)
+if model_is_saved:
+    alphabet = ['"', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', ':', 'A', 'B', 'C', 'E', 'F', 'H', 'I', 'J', 'K', 'L',
+            'R', 'S', 'U', 'W', 'Y', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z']
+    max_len = 16
+    alphabet_to_ind = layers.StringLookup(vocabulary=list(alphabet))
+    ind_to_alphabet = layers.StringLookup(vocabulary=alphabet_to_ind.get_vocabulary(), invert=True)
+else:
+    # Determina o comprimento da palavra mais longa e o total de símbolos no dataset:
+    texts = [file[1] for file in files]
+    words = [word for text in texts for word in text.split(' ')]
+    max_len = max([len(word) for word in words])
+    alphabet = set(c for word in words for c in word)
+    alphabet = sorted(list(alphabet))
+    print(alphabet)
+    print(max_len)
+    alphabet_to_ind = layers.StringLookup(vocabulary=list(alphabet))
+    ind_to_alphabet = layers.StringLookup(vocabulary=alphabet_to_ind.get_vocabulary(), invert=True)
 
-        model = models.load_model('modelo.keras')
-    else:
-        # Determina o comprimento da palavra mais longa e o total de símbolos no dataset:
-        texts = [file[1] for file in files]
-        words = [word for text in texts for word in text.split(' ')]
-        max_len = max([len(word) for word in words])
-        alphabet = set(c for word in words for c in word)
-        alphabet = sorted(list(alphabet))
-        print(alphabet)
-        print(max_len)
-        alphabet_to_ind = layers.StringLookup(vocabulary=list(alphabet))
-        ind_to_alphabet = layers.StringLookup(vocabulary=alphabet_to_ind.get_vocabulary(), invert=True)
+def encode_data(image, label):
+    image = layers.Rescaling(1.0/255)(image)
+    label = alphabet_to_ind(tf.strings.unicode_split(label, input_encoding='UTF-8'))
+    return {"image": image, "label" : label}
 
-        # Para cada um dos arquivos, carrega todos os retângulos encontrados e coloca
-        # junto do texto de referência.
-        X_dataset = []
-        y_dataset = []
-        for file in files:
-            img, closing, rectangles = load_img(file[0])
-            blocks = get_blocks(rectangles, block_dist_y[0], block_dist_x[0])
-            chars = get_chars(blocks[0], rectangles, word_dist_y[0], word_dist_x[0])
-            words = file[1].split(' ')
-            firsts = min(len(words), len(chars))
-            chars = chars[:firsts]
-            words = words[:firsts]
-            y_dataset = y_dataset + [word+(max_len - len(word))*' ' for word in words]
-            # Redimensiona a altura para 16 e o comprimento para 64
-            for rec in chars:
-                y = blocks[0][1]+rec[1]
-                x = blocks[0][0]+rec[0]
-                crop = closing[y:y+rec[3], x:x+rec[2]]
-                crop = cv.resize(crop, (height_input, width_input))
-                crop = np.array(crop.astype(np.uint8))
-                X_dataset.append(crop)
-        # Divide em conjunto de treino e de validação (0.7, 0.3)
-        split = round(len(X_dataset)*0.7)
-        X_train_set = np.array(X_dataset[:split])
-        y_train_set = np.array(y_dataset[:split])
-        X_valid_set = np.array(X_dataset[split:])
-        y_valid_set = np.array(y_dataset[split:])
-        
-        def encode_data(image, label):
-            image = layers.Rescaling(1.0/255)(image)
-            label = alphabet_to_ind(tf.strings.unicode_split(label, input_encoding='UTF-8'))
-            return {"image": image, "label" : label}
-
-        train_ds = tf.data.Dataset.from_tensor_slices((X_train_set, y_train_set))
-        train_ds = train_ds.map(encode_data, num_parallel_calls=tf.data.AUTOTUNE)
-        train_ds = train_ds.batch(batch_size)
-        train_ds = train_ds.prefetch(buffer_size = tf.data.AUTOTUNE)
-
-        valid_ds = tf.data.Dataset.from_tensor_slices((X_valid_set, y_valid_set))
-        valid_ds = valid_ds.map(encode_data, num_parallel_calls=tf.data.AUTOTUNE)
-        valid_ds = valid_ds.batch(batch_size)
-        valid_ds = valid_ds.prefetch(buffer_size = tf.data.AUTOTUNE)
+if model_is_saved:
+    model = models.load_model('modelo.keras')
+else:
+    # Para cada um dos arquivos, carrega todos os retângulos encontrados e coloca
+    # junto do texto de referência.
+    X_dataset = []
+    y_dataset = []
+    for file in files:
+        img, closing, rectangles = load_img(file[0])
+        blocks = get_blocks(rectangles, block_dist_y[0], block_dist_x[0])
+        chars = get_chars(blocks[0], rectangles, word_dist_y[0], word_dist_x[0])
+        words = file[1].split(' ')
+        firsts = min(len(words), len(chars))
+        chars = chars[:firsts]
+        words = words[:firsts]
+        y_dataset = y_dataset + [word+(max_len - len(word))*' ' for word in words]
+        # Redimensiona a altura para 16 e o comprimento para 64
+        for rec in chars:
+            y = blocks[0][1]+rec[1]
+            x = blocks[0][0]+rec[0]
+            crop = closing[y:y+rec[3], x:x+rec[2]]
+            crop = cv.resize(crop, (height_input, width_input))
+            crop = np.array(crop.astype(np.uint8))
+            X_dataset.append(crop)
+    # Divide em conjunto de treino e de validação (0.7, 0.3)
+    split = round(len(X_dataset)*0.7)
+    X_train_set = np.array(X_dataset[:split])
+    y_train_set = np.array(y_dataset[:split])
+    X_valid_set = np.array(X_dataset[split:])
+    y_valid_set = np.array(y_dataset[split:])
     
-        model = create_model(len(alphabet_to_ind.get_vocabulary()))
-        
-        checkpoint_path = 'training/cp-{epoch:04d}.weights.h5'
-        checkpoint_dir = 'training'
-        cp_callback =\
-        tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,\
-                save_weights_only=True, verbose=1)
-        
-        weights_dir = list(sorted(os.listdir(checkpoint_dir)))
-        last_epoch = 0
-        if len(weights_dir) > 0:
-            last_epoch_match = re.search(r'\d+', weights_dir[-1])
-            if last_epoch_match:
-                last_epoch = int(last_epoch_match.group(0))
-                model.load_weights(os.path.join(checkpoint_dir, weights_dir[-1]))
-        model.fit(train_ds, epochs=EPOCHS-last_epoch, validation_data=valid_ds,\
-                callbacks=[cp_callback], verbose=2)
-    model.summary()
-    prediction_model = models.Model(model.input[0],
-                                    model.get_layer(name = 'dense2').output)
-    return files, prediction_model
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train_set, y_train_set))
+    train_ds = train_ds.map(encode_data, num_parallel_calls=tf.data.AUTOTUNE)
+    train_ds = train_ds.batch(batch_size)
+    train_ds = train_ds.prefetch(buffer_size = tf.data.AUTOTUNE)
+
+    valid_ds = tf.data.Dataset.from_tensor_slices((X_valid_set, y_valid_set))
+    valid_ds = valid_ds.map(encode_data, num_parallel_calls=tf.data.AUTOTUNE)
+    valid_ds = valid_ds.batch(batch_size)
+    valid_ds = valid_ds.prefetch(buffer_size = tf.data.AUTOTUNE)
+
+    model = create_model(len(alphabet_to_ind.get_vocabulary()))
+    
+    checkpoint_path = 'training/cp-{epoch:04d}.weights.h5'
+    checkpoint_dir = 'training'
+    cp_callback =\
+    tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,\
+            save_weights_only=True, verbose=1, save_best_only=True)
+    
+    weights_dir = list(sorted(os.listdir(checkpoint_dir)))
+    last_epoch = 0
+    if len(weights_dir) > 0:
+        last_epoch_match = re.search(r'\d+', weights_dir[-1])
+        if last_epoch_match:
+            last_epoch = int(last_epoch_match.group(0))
+            model.load_weights(os.path.join(checkpoint_dir, weights_dir[-1]))
+    model.fit(train_ds, epochs=EPOCHS-last_epoch, validation_data=valid_ds,\
+            callbacks=[cp_callback], verbose=2)
+model.summary()
+prediction_model = models.Model(model.input[0],
+                                model.get_layer(name = 'dense2').output)
 
 def decode_batch_prediction(pred):
     input_len = np.ones(pred.shape[0]) * pred.shape[1]
-    decoded = tf.keras.backend.ctc_decode(pred, 
+    results = tf.keras.backend.ctc_decode(pred, 
                       input_length = input_len, 
-                                 greedy = True)
-    decoded = decoded[0][0][:, :max_len]
+                      greedy = True)[0][0][:, :max_len]
     output = []
-    for d in decoded:
+    for res in results:
         # Convert the predicted indices to the corresponding chars.
-        d = tf.strings.reduce_join(ind_to_alphabet(d))
-        d = d.numpy().decode("utf-8")
-        output.append(d)
+        res = tf.strings.reduce_join(ind_to_alphabet(res)).numpy().decode("utf-8")
+        output.append(res)
     return output
 
-def recognize(img, model):
-    # Erro...
-    img = img.astype('float32')
-    img /= 255.0
-    print(img.shape)
-    pred = model.predict(img)
-    return decode_batch_prediction(pred)
+def recognize(chars):
+    X_test = []
+    y_test = []
+    for rec in chars:
+        y = blocks[0][1]+rec[1]
+        x = blocks[0][0]+rec[0]
+        crop = closing[y:y+rec[3], x:x+rec[2]]
+        crop = cv.resize(crop, (height_input, width_input))
+        crop = np.array(crop.astype(np.uint8))
+        X_test.append(crop)
+        y_test.append("")
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+    test_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+    test_ds = test_ds.map(encode_data, num_parallel_calls= tf.data.AUTOTUNE)
+    test_ds = test_ds.batch(batch_size)
+    test_ds = test_ds.prefetch(buffer_size=tf.data.AUTOTUNE)
+    text = ""
+    for batch in test_ds:
+        batch_image = batch["image"]
+        pred = prediction_model.predict(batch_image)
+        words = decode_batch_prediction(pred)
+        for word in words:
+            text += word.strip("[UNK]") + ' '
+    print(text)
+    return text
 
 def main():
     figure_size = 320
@@ -356,8 +373,6 @@ def main():
     Y_COORDS = list(map(lambda c : int(c*scale_y), Y_COORDS))
     figure_size = int(figure_size*scale_y)
     
-    files, model = get_net()
-
     img, closing, rectangles = load_img(files[0][0])
     
     blocks = get_blocks(rectangles, block_dist_y[0], block_dist_x[0])
@@ -404,15 +419,8 @@ def main():
         elif pr.check_collision_point_rec(mouse_position, run_button):
             if pressed:
                 blocks = get_blocks(rectangles, block_dist_y[0], block_dist_x[0])
-                if len(blocks) > 0:
-                    chars = get_chars(blocks[0], rectangles, word_dist_y[0], word_dist_x[0])
-                    text = ""
-                    for rec in chars:
-                        y = blocks[0][1]+rec[1]
-                        x = blocks[0][0]+rec[0]
-                        crop = closing[y:y+rec[3], x:x+rec[2]]
-                        crop = cv.resize(crop, (height_input, width_input))
-                        text += recognize(crop, model) + ' '
+                chars = get_chars(blocks[0], rectangles, word_dist_y[0], word_dist_x[0])
+                text = recognize(chars)
             else:
                 background_color_button_run = pr.WHITE
                 border_color_run = pr.BLUE
